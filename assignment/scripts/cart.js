8f83e56b50eb27ebdf7fc9181d7c0b0a98218cd8
Ismail Ali <ishaliprime@gmail.com>
2023-04-21 17:32:01 -0500

    created addItem function and tested it

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,39 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+// - Create a global variable named `basket` and set it to an empty array.
+let basket = [];
+
+// - Create a function called `addItem`. It should:
+//   - take an input parameter for a string `item`
+//   - add the new item to the global array `basket`. 
+//   - return `true` indicating the item was added
+function addItem(item){
+    basket.push(item);
+    return true;
+}
+// Call and test
+console.log(`basket is: ${basket}`);
+console.log('Adding penny (expect true)', addItem('penny'));
+console.log('Adding nickels (expect true)', addItem('nickel'));
+console.log('Adding dime (expect true)', addItem('dime'));
+console.log('Adding quarter (expect true)', addItem('quarter'));
+
+
+
+console.log(`basket array: ${basket}`);
+
+// - Create a function called `listItems`. It should:
+//   - loop over the items in the `basket` array
+//   - console.log each individual item on a new line
+
+  
+
+//   - Create a function called `empty`. It should:
+//   - reset the `basket` to an empty array
+
+
+
+
+
